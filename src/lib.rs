1b2ac2e79197f80fc8228ce98a07886b461057a8
aleph-naught2tog <maggie@8thlight.com>
2019-08-18 16:44:37 -0500

    abc debug

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,25 @@ fn to_bits(input: &str) -> Vec<char> {
 }
 
 pub fn sha1(message: &str) -> (u32, u32, u32, u32, u32) {
-    let mut as_bits = to_bits(message);
+    let m = String::from("abc") + "1";
+    let mut as_bits = to_bits(&m);
 
     let mut h0: u32 = 0x6745_2301;
     let mut h1: u32 = 0xEFCD_AB89;
     let mut h2: u32 = 0x98BA_DCFE;
     let mut h3: u32 = 0x1032_5476;
     let mut h4: u32 = 0xC3D2_E1F0;
+    // 3 = (x / 4) + 2
+    // 3 - 2 = x / 4;
+    // 1 = x / 4;
+    // x = 4;
+    // as_bits.append(&mut vec![char::from(0x80)]);
+    // as_bits.append(&mut "10000000".chars().collect::<Vec<char>>());
+    let message_length: u64 = m.len() as u64;
+    println!("message length after conv: {}", message_length / 4 + 2);
+    let mut len_as_bits = format!("{:064b}", message_length).chars().collect::<Vec<char>>();
+    // println!("message length as bits: {}", len_as_bits);
 
-    let m1: u64 = as_bits.len() as u64;
-    let mut len_as_bits = format!("{:064b}", m1).chars().collect::<Vec<char>>();
-
-    as_bits.append(&mut vec!['1']);
 
     while 512 - (as_bits.len() as u32 % 512) != 64 {
         as_bits.append(&mut vec!['0']);
@@ -78,21 +85,25 @@ pub fn sha1(message: &str) -> (u32, u32, u32, u32, u32) {
                     //    f = (b and c) or ((not b) and d)
                     f = (b & c) | ((!b) & d);
                     k = 0x5A82_7999;
+                    // 0x5a82_7999,
                 }
                 _ if index <= 39 => {
                     // f = b xor c xor d
                     f = b ^ c ^ d;
                     k = 0x6ED9_EBA1;
+                    // 0x6ed9_eba1,
                 }
                 _ if index <= 59 => {
                     // f = (b and c) or (b and d) or (c and d)
                     f = (b & c) | (b & d) | (c & d);
                     k = 0x8F1B_BCDC;
+                    // 0x8f1b_bcdc,
                 }
                 _ if index <= 79 => {
                     // f = b xor c xor d
                     f = b ^ c ^ d;
                     k = 0xCA62_C1D6;
+                    // 0xca62_c1d6
                 }
                 _ => panic!("Rust broke..."),
             }
@@ -133,14 +144,12 @@ mod tests {
 
     #[test]
     fn it_works() {
-        // 2fd4e1c67a2d28fced849ee1bb76e7391b93eb12
-        let input = "The quick brown fox jumps over the lazy dog";
+        let input = "abc";
         let output = sha1(input);
 
         println!("{:#?}", output);
 
-        let (a,b,c,d,e) = output;
-        // 31d70e67c2ecd0075746ca81bf6cfeb16509aaef
+        let (a, b, c, d, e) = output;
         println!("{:x}{:x}{:x}{:x}{:x}", a, b, c, d, e);
     }
 }
